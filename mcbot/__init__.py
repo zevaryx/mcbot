8c761f6e278c86d0f144c885b6bb3d0cecf0f9e7
zevaryx <zevaryx@gmail.com>
2026-03-24 00:46:58 +0000

    fix: Move __version__ to const.py

--- mcbot/__init__.py
+++ mcbot/__init__.py
@@ -3,5 +3,3 @@ from .settings import Settings, load_settings
 from .models.internal import triggers
 from .models.internal.task import Task
 from .models.internal.context import Context
-
-__version__ = "0.1.0"
