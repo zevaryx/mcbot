924d17c36b6d1d1d97a01b608e2a43bdefb4ed12
zevaryx <zevaryx@gmail.com>
2026-03-24 15:08:44 +0000

    fix: Context not checking for valid packets before processing

--- mcbot/models/internal/context.py
+++ mcbot/models/internal/context.py
@@ -12,11 +12,15 @@ class Context:
         self.packet = packet
         self.type = packet.get_payload_type()
         self.data = packet.decrypted.get("group_text_data")
+        if not self.data:
+            raise ValueError("Invalid packet")
         self.sender = self.data.get("sender_name")
         self.content = ": ".join(self.data.get("full_content").split(": ")[1:])
         self.command = self.content.split("/")[1].split(" ")[0]
         self.channel_name = self.data.get("channel_name")
         self.channel = self.bot.channels.find_by_name(self.channel_name)
+        if not self.channel:
+            raise ValueError("Could not find channel!")
         
     async def reply(self, text: str):
         full_text = f"@[{self.sender}] {text}"
@@ -27,5 +31,5 @@ class Context:
     async def send(self, text: str):
         if len(text) > 140:
             raise ValueError("Length of text cannot exceed 140 characters")
-        await self.bot.send_channel_message(self.channel, text)
+        await self.bot.send_channel_message(self.channel, text) # type: ignore
         
