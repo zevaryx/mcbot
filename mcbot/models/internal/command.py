02896f156bf741e9cf09ef32fa41fb23b77567a1
zevaryx <zevaryx@gmail.com>
2026-03-25 04:49:52 +0000

    feat: Add extension support

--- mcbot/models/internal/command.py
+++ mcbot/models/internal/command.py
@@ -1,4 +1,7 @@
+import inspect
 from typing import Callable, Any, Awaitable
+
+from mcbot.settings import load_settings
 
 CallbackType = Callable[..., Awaitable[Any]]
 
@@ -11,3 +14,35 @@ class Command:
         
     async def dispatch(self, *args, **kwargs) -> Any:
         await self.callback(*args, **kwargs)
+        
+def command(
+    name: str = "",
+    *,
+    description: str = "", 
+    help: str = ""
+) -> Callable[[CallbackType], Command]:
+    """Create a new command.
+    
+    Usage:
+    ```
+    bot = Bot(settings)
+    
+    @bot.command(description="Pong!")
+    async def ping(ctx):
+        await ctx.send("Pong!")
+    ```
+    
+    Args:
+        callback: Function to call on command execution
+    """
+    def wrapper(func: CallbackType) -> Command:
+        settings = load_settings()
+        if not inspect.iscoroutinefunction(func):
+            raise ValueError("Commands must be coroutines!")
+        
+        _name = name or func.__name__
+        _description = description or func.__doc__ or "No description"
+        _help = description or help or settings.prefix + _name
+        cmd = Command(_name, func, _description, _help)
+        return cmd
+    return wrapper
