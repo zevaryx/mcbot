3e8fbe35439ba50466e4bfa912838e0322b10c62
zevaryx <zevaryx@gmail.com>
2026-03-25 04:22:28 +0000

    feat: More detailed command function decorator

--- mcbot/models/internal/command.py
+++ mcbot/models/internal/command.py
@@ -3,9 +3,11 @@ from typing import Callable, Any, Awaitable
 CallbackType = Callable[..., Awaitable[Any]]
 
 class Command:
-    def __init__(self, name: str, callback: CallbackType):
+    def __init__(self, name: str, callback: CallbackType, description: str = "", help: str = ""):
         self.name = name
         self.callback = callback
+        self.description = description or callback.__doc__
+        self.help = help
         
     async def dispatch(self, *args, **kwargs) -> Any:
         await self.callback(*args, **kwargs)
