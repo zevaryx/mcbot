8c8a58d21989881c19ba8de944748c4ece75cadb
zevaryx <zevaryx@gmail.com>
2026-03-26 17:25:13 +0000

    fix: Import error

--- mcbot/__init__.py
+++ mcbot/__init__.py
@@ -1,5 +1,5 @@
 from .client import Bot
-from .settings import Settings, load_settings
+from .settings import Settings
 from .models.internal import triggers
 from .models.internal.task import Task
 from .models.internal.context import Context
